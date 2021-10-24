53a280b57925ad7aa39c5ca25f11045fc86d9d4f
ips21blank <inderpalsingh423@gmail.com>
2021-10-24 21:14:54 +0530

    Initial setup 1.0

---
+++ webpack/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        exclude: /node_modules/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/i,
+        exclude: /node_modules/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.scss$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+};
---
+++ webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+
+module.exports = (envVars) => {
+  const { env } = envVars;
+  const envConfig = require(`./webpack.${env}`);
+  const config = merge(common, envConfig);
+
+  return config;
+};
