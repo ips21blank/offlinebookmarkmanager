36e34d25261e4bcbfdf5dc5fd9d888cbba1e722d
blank blank <inderpalsingh423@gmail.com>
2022-05-21 13:34:46 +0530

    Move scripts from App.tsx to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { createAndGetStore } from '@redux/redux';
+import {
+  addListenersToBrowser,
+  browserAPI,
+  addCustomDragEvents,
+  testFn
+} from '@scripts/scripts';
+import { TIMESTAMP } from '@scripts/globals';
 
-(async function renderApp() {
+(async function renderAppAndStartup() {
   const store = await createAndGetStore();
 
   ReactDOM.render(
@@ -12,4 +19,17 @@ import { createAndGetStore } from '@redux/redux';
     </React.StrictMode>,
     document.getElementById('root')
   );
+
+  // Add code for firing and listning to custom events.
+  addCustomDragEvents();
+
+  // Add listeners to browser bookmark/storage/... events.
+  addListenersToBrowser();
+
+  // Timestamp for app started. If the following timpstamp.
+  // does not match on storage changes, tab is closed.
+  browserAPI.store({
+    [TIMESTAMP.key]: TIMESTAMP.val
+  });
+  testFn();
 })();
